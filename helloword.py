0e847adde4c6661470f3c9978b1584b875bf8487
Joe Tam <someemails@gmail.com>
2022-04-27 01:36:52 -0700

    merge2

--- helloword.py
+++ helloword.py
@@ -1,18 +1,9 @@
-<<<<<<< HEAD
 def hello_world(city, state):
     print(f'Hello World {city}, {state}!')
 
 
 def main():
     hello_world('NYC','New York')
-=======
-def hello_world(city):
-    print(f'Hello World! {city}')
-
-
-def main():
-    hello_world('NYC')
->>>>>>> c4d54d260c6738f8b37bbf8bc24ea3861a3d72c9
 
 
 main()
